f1f612db209a9f89c5661280a1a7cd5945db6ddb
whong92 <wai.ong11@alumni.imperial.ac.uk>
2019-03-10 23:04:56 +0000

    bugfix : mean calculated only over set entries

--- recommenderCF/recommenderCFSimple.py
+++ recommenderCF/recommenderCFSimple.py
@@ -1,5 +1,5 @@
 from ..recommenderInterface import Recommender
-from ..utils.utils import csv2df, df2umCSR, rmse
+from ..utils.utils import csv2df, df2umCSR, rmse, mean_nnz
 from sklearn.metrics.pairwise import cosine_similarity
 import scipy.sparse as sps
 import numpy as np
@@ -45,22 +45,22 @@ class RecommenderCFSimple(Recommender):
         k = self.k
         self.S = cosine_similarity(Ucsr) # can be replaced by locality-sensitive hashing
 
-        mu = np.array(np.mean(Ucsr))
-        bx = np.squeeze(np.array(np.mean(Ucsc, axis=0))) - mu
-        bi = np.squeeze(np.array(np.mean(Ucsr, axis=1))) - mu
+        mu = np.array(mean_nnz(Ucsr))
+        bx = mean_nnz(Ucsc, axis=0, mu=mu) - mu
+        bi = mean_nnz(Ucsr, axis=1, mu=mu) - mu
 
         self.mu = mu
         self.bx = bx
         self.bi = bi
 
         if precompute_ud: # pre-computation, expensive!
+
             self.Ud = np.zeros(shape=(M, N))
             for u in range(N):
                 ss = Ucsc[:,u].nonzero()[0] #row numbers
                 ratings = np.array(Ucsc[ss,u].todense())[:,0]
                 bxi = mu + bx[u] + bi
                 bxj = bxi[ss]
-
                 if self.S[:, ss].shape[1] >= k:
                     kidx = np.argpartition(self.S[:, ss], self.S[:, ss].shape[1]-k+1)[:,-k:]
                     ratings = ratings[kidx]
@@ -73,7 +73,6 @@ class RecommenderCFSimple(Recommender):
                 nzi = np.sum(Sij,axis=1) > 0
                 self.Ud[nzi,u] = bxi[nzi] + np.divide(T[nzi],np.sum(Sij[nzi,:], axis=1))
                 self.Ud[~nzi, u] = bxi[~nzi]
-                self.Ud[:,u] = bxi
 
         test_idx = Vcsr.nonzero()
         test_error = rmse(self.predict(test_idx[0], test_idx[1]), Vcsr[test_idx[0], test_idx[1]])
@@ -83,6 +82,7 @@ class RecommenderCFSimple(Recommender):
     def predict(self, items, users, **kwargs):
 
         assert len(users) == len(items)
+
         rpred = np.zeros(shape=(len(users),))
 
         if self.Ud is None:
@@ -118,5 +118,5 @@ if __name__=="__main__":
         rcf.from_csv('D:/PycharmProjects/recommender/data/ml-latest-small/ratings.csv', 'userId', 'movieId', 'rating')
         test_error, es = rcf.train(precompute_ud=True)
         print("k : {0} , test error {1}".format(k, test_error))
-        #print(plt.hist(es, bins=50))
+        #plt.hist(es, bins=50)
         plt.show()
