f17c5f1b959fead9921cc46543849ab038a3e577
whong92 <wai.ong11@alumni.imperial.ac.uk>
2019-03-16 23:14:37 +0000

    implementation and testing of cosine similarity hash

--- hash/cosineSimHash.py
+++ hash/cosineSimHash.py
@@ -1,20 +1,64 @@
 import numpy as np
 import scipy.sparse as sps
-from .minhash import Signature, MinHashSignature, NUM_BUCKETS
+from .minhash import Signature, MinHashSignature
+from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import jaccard
 
-class cosineSimHash(Signature):
+class CosineSimHash(Signature):
 
-    def __init__(self, num_hpp=300 , num_minhash=100):
+    def __init__(self, num_hpp=300):
         self.num_hpp = num_hpp
-        self.msh = MinHashSignature(num_minhash)
 
-    def generate_signature(self, X):
+    def generate_signature(self, X, new_hash=True):
         Nhpp = self.num_hpp
         R = X.shape[0]
         C = X.shape[1]
         B = np.zeros(shape=(Nhpp,C), dtype=bool)
         XT = X.transpose()
         XT = XT.tocsr()
+        """
         for i in range(Nhpp):
-            hpp = np.random.normal(0, 1.0, (R))
+            hpp = np.random.normal(0, 1.0, R)
             B[i,:] = XT*hpp > 0
+        """
+        hpp = np.random.normal(0, 1.0, (R,Nhpp))
+        B = XT * hpp > 0
+        return B.transpose()
+
+
+if __name__=="__main__":
+
+    N = 5000
+    M = 100
+    Hpp = 300
+    NumElems = np.array([10000, 20000, 50000])
+
+    for E in NumElems:
+
+        rows = np.random.randint(0, M, E)
+        cols = np.random.randint(0, N, E)
+        data = np.random.normal(0, 1.0, E)
+        X = sps.csc_matrix((data,(rows, cols)),shape=(M,N))
+        csh = CosineSimHash(num_hpp=Hpp)
+        S = csh.generate_signature(X)
+
+        XT = X.transpose()
+        XT = XT.tocsr()
+
+        cs = cosine_similarity(XT)
+        countMinHash = np.zeros(shape=(N-1,))
+        theta = np.zeros(shape=(N - 1,))
+
+        for i in range(1,N):
+            if i == 0:
+                continue
+            countMinHash[i-1] = float(np.sum(np.array(S[:,0]==S[:,i]).astype(int)))/Hpp
+            theta[i-1] = 1 - np.arccos(cs[0,i])/np.pi
+
+        plt.plot(theta, countMinHash, 'o', label='{}'.format(E))
+
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.legend()
+    plt.show()
--- utils/testutils.py
+++ utils/testutils.py
@@ -1,4 +1,6 @@
 from .utils import *
+from sklearn.decomposition import PCA, TruncatedSVD
+import matplotlib.pyplot as plt
 
 def test_csv2df():
     df,_,_ = csv2df('D:/PycharmProjects/recommender/data/ml-latest-small/ratings.csv', 'userId', 'movieId', 'rating')
@@ -9,6 +11,16 @@ def test_csv2umCSR():
     um = df2umCSR(df)
     print(um[:30,:30])
 
+def test_csv2PCA():
+    df, _, _ = csv2df('D:/PycharmProjects/recommender/data/ml-latest-small/ratings.csv', 'userId', 'movieId', 'rating')
+    um = df2umCSR(df)
+    print(um.shape)
+    pca = PCA()
+    pca.fit(um.todense())
+    plt.plot(pca.explained_variance_, 'o')
+    plt.show()
+
 if __name__ == "__main__":
-    test_csv2df()
-    test_csv2umCSR()
+    #test_csv2df()
+    #test_csv2umCSR()
+    test_csv2PCA()
