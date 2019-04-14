581a9e920e9244d999b3d5fb5779b8a9cfda5db2
whong92 <wai.ong11@alumni.imperial.ac.uk>
2019-04-15 00:17:06 +0100

    cleaning up interface

--- recommender/recommenderMF.py
+++ recommender/recommenderMF.py
@@ -14,8 +14,9 @@ import os
 
 class recommenderMF(Recommender):
 
-    def __init__(self, n_users, n_items, mode='train'
+    def __init__(self, mode='train'
                  # arguments for train mode
+                 , n_users=None, n_items=None
                  , signature_t='CosineHash', lsh_t='LSHSimple'
                  , mf_kwargs=None, signature_kwargs=None, lsh_kwargs=None
                  # arguments for predict mode
@@ -26,42 +27,42 @@ class recommenderMF(Recommender):
         self.mode = mode
         self.estimator = None
         self.predictor = None
-        self.n_users = n_users
-        self.n_items = n_items
         # only for training
-        self.signature_t = signature_t
-        self.lsh_t = lsh_t
-        self.mf_kwargs = mf_kwargs
-        self.signature_kwargs = signature_kwargs
-        self.lsh_kwargs = lsh_kwargs
+        self.config = {
+            'n_users': n_users, 'n_items': n_items
+            , 'signature_t': signature_t, 'lsh_t': lsh_t
+        }
         self.lsh = None
+        self.sig = None
+
+        if mode is 'train':
+            if mf_kwargs is None:
+                mf_kwargs = {'f': 20, 'lamb': .001, 'lr': .01, 'decay': 0.0}
+            self.estimator = MatrixFactorizer(n_users, n_items, **mf_kwargs)
+            self._make_hash(signature_t, lsh_t, signature_kwargs, lsh_kwargs)
 
         if mode is 'predict':
 
             for x in [model_path, lsh_path]:
                 assert x is not None
 
-            name_path = os.path.join(lsh_path, 'names.json')
+            config_path = os.path.join(lsh_path, 'config.json')
             sig_path = os.path.join(lsh_path, 'signature.json')
             hash_path = os.path.join(lsh_path, 'hash.json')
 
-            with open(name_path, 'r', encoding='utf-8') as fp:
-                names = json.load(fp)
+            with open(config_path, 'r', encoding='utf-8') as fp:
+                self.config = json.load(fp)
 
-            self.signature_t = names['sig']
-            self.lsh_t = names['lsh']
-            sig = MakeSignature(self.signature_t, path=sig_path)
-            self.lsh = MakeLSH(self.lsh_t, sig, path=hash_path)
+            signature_t = self.config['signature_t']
+            lsh_t = self.config['lsh_t']
+            self.sig = MakeSignature(signature_t, path=sig_path)
+            self.lsh = MakeLSH(lsh_t, self.sig, path=hash_path)
             self.predictor = predictor.from_saved_model(model_path)
 
-    def train(self, u_train, i_train, r_train, u_test, i_test, r_test, model_path=None, lsh_path=None):
+    def train(self, u_train, i_train, r_train, u_test, i_test, r_test,
+              model_path=None, lsh_path=None):
 
         assert self.mode is 'train', "must be in train mode!"
-
-        if self.mf_kwargs is None:
-            self.mf_kwargs = {'f': 20, 'lamb': .001, 'lr': .01, 'decay': 0.0}
-
-        self.estimator = MatrixFactorizer(self.n_users, self.n_items, **self.mf_kwargs)
 
         self.estimator.fit(
             {'u_in': u_train, 'i_in': i_train}, r_train,
@@ -70,13 +71,23 @@ class recommenderMF(Recommender):
         )
 
         # generate embeddings for all items, and insert into LSH
-        self._make_and_update_hash(lsh_path, self.signature_t, self.lsh_t
-                                  , self.signature_kwargs, self.lsh_kwargs, None)
+        self._update_hash(None)
         if model_path is not None:
             self.estimator.save(model_path)
+        if lsh_path is not None:
+            self._save_hash(lsh_path)
 
-    def _make_and_update_hash(self, lsh_path=None, signature_t='CosineHash', lsh_t='LSHSimple'
-                             , signature_kwargs=None, lsh_kwargs=None, items=None):
+    def _make_hash(self, signature_t='CosineHash', lsh_t='LSHSimple',
+                   signature_kwargs=None, lsh_kwargs=None):
+        if signature_kwargs is None:
+            signature_kwargs = {'num_row': 20, 'num_hpp': 100}
+        if lsh_kwargs is None:
+            lsh_kwargs = {'num_bands':5}
+
+        self.sig = MakeSignature(signature_t, **signature_kwargs)
+        self.lsh = MakeLSH(lsh_t, self.sig, **lsh_kwargs)
+
+    def _update_hash(self, items=None):
         # rebuild lsh
         if self.mode is 'train':
             # TODO: use predict mode in Matrix Factorizer
@@ -85,28 +96,19 @@ class recommenderMF(Recommender):
             pred = self.predictor
 
         if items is None:
-            items = np.arange(0, self.n_items, dtype=np.int32)
+            items = np.arange(0, self.config['n_items'], dtype=np.int32)
 
         p = pred(
             {'u_in': np.zeros(shape=(len(items),), dtype=np.int32),
              'i_in': items
              })
-
-        if signature_kwargs is None:
-            signature_kwargs = {'num_row': 20, 'num_hpp': 100}
-        if lsh_kwargs is None:
-            lsh_kwargs = {'num_bands':5}
-
-        sig = MakeSignature(signature_t, **signature_kwargs)
-        self.lsh = MakeLSH(lsh_t, sig, **lsh_kwargs)
         self.lsh.insert(sps.csc_matrix(p['q'].transpose()),Xindex=items)
 
-        if lsh_path is not None:
-            names = {'sig': self.signature_t, 'lsh': self.lsh_t}
-            with open(os.path.join(lsh_path, 'names.json'), 'w', encoding='utf-8') as fp:
-                json.dump(names, fp)
-            sig.save(os.path.join(lsh_path, 'signature.json'))
-            self.lsh.save(os.path.join(lsh_path, 'hash.json'))
+    def _save_hash(self, lsh_path):
+        with open(os.path.join(lsh_path, 'config.json'), 'w', encoding='utf-8') as fp:
+            json.dump(self.config, fp)
+        self.sig.save(os.path.join(lsh_path, 'signature.json'))
+        self.lsh.save(os.path.join(lsh_path, 'hash.json'))
 
     def predict(self, u_in, i_in):
 
@@ -130,8 +132,8 @@ class recommenderMF(Recommender):
 
     def recommend(self, u_in):
         # manual prediction by enumerating all stuff
-        p = self.predictor({'u_in': np.tile(np.array([u_in], dtype=np.int32), self.n_items),
-                            'i_in': np.arange(0, self.n_items, dtype=np.int32)})
+        p = self.predictor({'u_in': np.tile(np.array([u_in], dtype=np.int32), self.config['n_items']),
+                            'i_in': np.arange(0, self.config['n_items'], dtype=np.int32)})
 
         return np.argsort(p['rhat']), np.sort(p['rhat'])
 
@@ -156,7 +158,7 @@ if __name__=="__main__":
     Items_test = D_test['item']
     Ratings_test = D_test['rating']
 
-    rmf = recommenderMF(N, M, mode='train')
+    rmf = recommenderMF(mode='train', n_users=N, n_items=M)
     tf.logging.set_verbosity(tf.logging.INFO)
     rmf.train(
         np.array(Users_train, dtype=np.int32),
@@ -165,21 +167,23 @@ if __name__=="__main__":
         np.array(Users_test, dtype=np.int32),
         np.array(Items_test, dtype=np.int32),
         np.array(Ratings_test, dtype=np.float64),
-        model_path = './bla',
-        lsh_path = './bla'
+        model_path='./bla',
+        lsh_path='./bla'
     )
     """
 
     # prediction
     df_mov = pd.read_csv('D:/PycharmProjects/recommender/data/ml-latest-small/movies.csv')
-    rmf = recommenderMF(N, M, mode='predict', model_path='./bla/1555277760', lsh_path='./bla')
+    rmf = recommenderMF(mode='predict', model_path='./bla/1555283393', lsh_path='./bla')
 
     thing = df.groupby('item').count()['user']
     thing = thing.loc[thing > 10]
 
-    #rmf._make_and_update_hash(lsh_path='./bla', items=thing.index)
+    rmf._make_hash()
+    rmf._update_hash(items=thing.index)
+    rmf._save_hash('./bla')
 
-    user = 22
+    user = 55
     user_df = df.loc[df.user_cat==user]
 
     pred = rmf.predict(np.array(user_df['user']), np.array(user_df['item']))
@@ -215,6 +219,9 @@ if __name__=="__main__":
 
     print(sorted_items.merge(df_mov, left_on='item', right_on='movieId')[-10:])
     print(user_df.sort_values('rating').merge(df_mov, left_on='item', right_on='movieId')[-10:])
+
+
+
 
 
 
