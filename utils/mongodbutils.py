b5ff6dca7376812a05a20cbe0f4cb7dceb658bed
whong92 <wai.ong11@alumni.imperial.ac.uk>
2019-05-01 09:07:02 +0800

    stuff

--- utils/mongodbutils.py
+++ utils/mongodbutils.py
@@ -13,6 +13,13 @@ def attempt_bulk_write(table, query):
         raise Exception("Error doing build write: {:s}".format(bwe.details))
     return result.acknowledged
 
+def attempt_insert_many(table, docs):
+    try:
+        result = table.insert_many(docs)
+    except pm.errors.PyMongoError as pe:
+        raise Exception("Pymongo error from insertion operation: {:s}".format(pe.details))
+    return result.acknowledged
+
 class DataService:
 
     def __init__(self, conn, db):
@@ -20,18 +27,9 @@ class DataService:
          self.db = self.dbclient[db]
 
     def insert_features(self, table, features):
-
         assert len(features) > 0, 'no features to insert'
-
         feat_table = self.db["features/{:s}".format(table)]
-        query = [
-            pm.InsertOne(
-                {'_id': int(f), 'feature': list(features[f])},
-            )
-            for f in features
-        ]
-
-        return attempt_bulk_write(feat_table, query)
+        return attempt_insert_many(feat_table, [{'_id': int(f), 'feature': list(features[f])} for f in features])
 
     def update_band(self, table, band_num, bucket):
 
